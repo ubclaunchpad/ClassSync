804d6cff25f4c20d1fe201bb9a106183fe5511ae
jjessieshang <17jessies@gmail.com>
2023-09-27 18:23:55 -0700

    student registration framework

--- backend/src/controllers/studentProfileController.js
+++ backend/src/controllers/studentProfileController.js
@@ -1,6 +1,40 @@
 // Methods called from the route are here, any db calls are made from here to the queries in models
+import { StudentProfile } from "../models/studentProfile.js";
 
 export default class studentProfileController {
+    saveStudentProfile(req) {
+        return new Promise((resolve, reject) => {
+            const studentProfileModel = new StudentProfile();
 
+            const studentProfile = {
+                student_id: req.body.student_id,
+                first_name: req.body.first_name,
+                last_name: req.body.last_name,
+                birthday: req.body.birthday,
+                accomodations: req.body.accomodations,
+                neurodivergent: req.body.neurodivergent,
+                fk_parent_id: req.body.fk_parent_id
+            };
+
+            studentProfileModel.insertStudentProfile(studentProfile, (err, result) => {
+                if (err) {
+                reject({ error: err });
+                }
+                resolve(result);
+            });
+        });
+    }
+
+      deleteStudentProfile(id) {
+        return new Promise((resolve, reject) => {
+            const studentProfileModel = new StudentProfile();
+    
+            studentProfileModel.deleteStudentProfile(id, (err, result) => {
+                if (err) {
+                reject({ error: err });
+                }
+                resolve(result);
+            });
+        });
+    }
 }
-
--- backend/src/routes/studentProfileRoute.js
+++ backend/src/routes/studentProfileRoute.js
@@ -1,10 +1,39 @@
 import { Router } from "express";
-const router = Router();
+import StudentProfileController from "../controllers/studentProfileController.js";
 
+const router = Router();
+const studentProfileController = new StudentProfileController();
 
 router.get("/pingcheck", authorize(), (_, res) => {
     res.status(200).json({ message: "pong" });
 });
 
+router.post("/", authorize(), (req, res) => {
+    if (!req.body) {
+      res.status(400).send({
+        message: "Content can not be empty!",
+      });
+      return;
+    }
+    studentProfileController
+      .saveStudentProfile(req)
+      .then((response) => {
+        res.status(200).json(response);
+      })
+      .catch((err) => {
+        res.status(404).json(err);
+      });
+  });
+
+  router.delete("/:studentId", authorize(), (req, res) => {
+    studentProfileController
+      .deleteStudentProfile(req.params.studentId)
+      .then((response) => {
+        res.status(200).json(response);
+      })
+      .catch((err) => {
+        res.status(404).json(err);
+      });
+  });
 
 export default router;
