948fdcadf577b5a6e20028f558f8b0d26d8f5ebb
tsomani <tabreek.somani@gmail.com>
2023-09-28 20:13:41 -0700

    Volunteer Registration Model Outline

---
+++ backend/src/models/volunteerRegistration.js
@@ -0,0 +1,18 @@
+export class volunteerRegistration {
+
+    updateStatus(userID, status) {
+        // activate account for user
+    }
+    setPassword(userID, password) {
+        // set password for user
+    }
+
+    updateBio(userID, bio) {
+        // update bio for user
+        // bio consists of first name, last name, date of birth, about me, profile picture
+    }
+
+    updateOfferings(userID, offerings) {
+
+    }
+}
