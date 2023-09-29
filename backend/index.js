0d2e42a23a86178ce6b081ee919ca84247515c72
tsomani <tabreek.somani@gmail.com>
2023-09-28 20:15:29 -0700

    Added volunteer account route

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import studentProfileRouter from "./src/routes/studentProfileRoute.js";
+import volunteerRegistrationRouter from "./src/routes/volunteerRegistrationRoute.js";
+
 
 const app = express();
 const port = 8080;
@@ -13,6 +15,8 @@ app.use(cors());
 app.get("/", (_, res) => {
     res.send("Hello ClassSync!");
 });
+
+app.use("/volunteer", volunteerRegistrationRouter);
 
 
 app.use("/student-profile", studentProfileRouter);
