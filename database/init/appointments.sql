d4ed9a62b42262f49e21d74adcb66fbead4239ee
tsomani <tabreek.somani@gmail.com>
2023-10-05 08:30:15 -0700

    Fixed Syntax

--- database/init/appointments.sql
+++ database/init/appointments.sql
@@ -8,13 +8,12 @@ DELIMITER $$
 CREATE PROCEDURE `createAppointments` ()
 BEGIN
 CREATE TABLE `appointments` (
-    appointment_id INT NOT NULL AUTO_INCREMENT,
+    appointment_id INT AUTO_INCREMENT PRIMARY KEY,
 	student_id VARCHAR(50) NOT NULL,
 	tutor_id VARCHAR(50) NOT NULL,
 	course_id VARCHAR(50) NOT NULL,
     appointment_start DATETIME NOT NULL,
     duration INT NOT NULL,
-	PRIMARY KEY (appointment_id),
 	FOREIGN KEY (student_id) REFERENCES students(student_id),
     FOREIGN KEY (tutor_id) REFERENCES tutors(tutor_id),
     FOREIGN KEY (course_id) REFERENCES courses(course_id)
