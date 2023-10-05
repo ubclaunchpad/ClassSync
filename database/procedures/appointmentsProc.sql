81963a72ffa0d2b68b1f1cb72e652d19e48bedeb
tsomani <tabreek.somani@gmail.com>
2023-10-05 08:16:45 -0700

    Multiple Get Appointment Procedures added

--- database/procedures/appointmentsProc.sql
+++ database/procedures/appointmentsProc.sql
@@ -54,4 +54,82 @@ BEGIN
     DELETE FROM appointments WHERE appointment_id = id;
 END $$
 
+
+
+CREATE PROCEDURE `getAppointmentsByTutor` (
+    IN tutor_id VARCHAR(50)
+) 
+BEGIN
+    SELECT * FROM appointments WHERE tutor_id = tutor_id;
+END$$
+
+CREATE PROCEDURE `getAppointmentsBYStudent` (
+    IN student_id VARCHAR(50)
+)
+BEGIN
+    SELECT * FROM appointments WHERE student_id = student_id;
+END$$
+
+CREATE PROCEDURE `getAppointmentsByCourse` (
+    IN course_id VARCHAR(50)
+)
+BEGIN
+    SELECT * FROM appointments WHERE course_id = course_id;
+END$$
+
+CREATE PROCEDURE `getAppointmentsInWeek` (
+    IN startdate DATETIME,
+    IN enddate DATETIME
+)
+BEGIN
+    SELECT * FROM appointments WHERE appointment_start BETWEEN startdate AND enddate;
+END$$
+
+CREATE PROCEDURE `getAllAppointments` ()
+BEGIN
+    SELECT * FROM appointments;
+END$$
+CREATE PROCEDURE `deleteAppointment`  (
+    IN id INT
+)
+BEGIN
+    DELETE FROM appointments WHERE appointment_id = id;
+END $$
+
+
+CREATE PROCEDURE `getAppointmentsByTutor` (
+    IN tutor_id VARCHAR(50)
+) 
+BEGIN
+    SELECT * FROM appointments WHERE tutor_id = tutor_id;
+END$$
+
+CREATE PROCEDURE `getAppointmentsByStudent` (
+    IN student_id VARCHAR(50)
+)
+BEGIN
+    SELECT * FROM appointments WHERE student_id = student_id;
+END$$
+
+CREATE PROCEDURE `getAppointmentsByCourse` (
+    IN course_id VARCHAR(50)
+)
+BEGIN
+    SELECT * FROM appointments WHERE course_id = course_id;
+END$$
+
+CREATE PROCEDURE `getAppointmentsInWeek` (
+    IN startdate DATETIME,
+    IN enddate DATETIME
+)
+BEGIN
+    SELECT * FROM appointments WHERE appointment_start BETWEEN startdate AND enddate;
+END$$
+
+CREATE PROCEDURE `getAllAppointments` ()
+BEGIN
+    SELECT * FROM appointments;
+END$$
+
+
 DELIMITER ;
