8b2da329ca0f00b91fcd3ce9b7b9e9eb090ca60e
Sue Lee <suuelee@outlook.com>
2023-10-06 00:25:50 -0700

    Add createUsers() sproc

---
+++ database/init/users.sql
@@ -0,0 +1,19 @@
+CREATE OR REPLACE PROCEDURE createUsers()
+
+LANGUAGE plpgsql 
+AS $$
+
+BEGIN
+IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname='userEnum') 
+THEN CREATE TYPE userEnum AS ENUM ('admin', 'user', 'guardian');
+END IF;
+
+CREATE TABLE "users" (
+    user_id SERIAL NOT NULL PRIMARY KEY,
+    role userEnum NOT NULL,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL
+);
+
+END;
+$$;
