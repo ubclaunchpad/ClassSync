7f36860e41f4fbe44ecdba49ac96ce0071f4b8f9
jjessieshang <17jessies@gmail.com>
2023-10-09 14:04:15 -0700

    routes to different screens

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./screens/login";
+import AddStudent from "./screens/addStudent";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+return (
+  <div className="App">
+    <BrowserRouter>
+      <Routes>  
+        <Route path="/login" element={<Login />} />
+        <Route path="/addStudent" element={<AddStudent />} />
+      </Routes>
+    </BrowserRouter>
+  </div>
+);
 }
 
 export default App;
---
+++ frontend/src/screens/addStudent/index.js
@@ -0,0 +1,15 @@
+import Header from "../../components/Header";
+import Banner from "../../components/Banner";
+import UserForm from "../../components/UserForm";
+import "./index.css";
+
+const AddStudent = () => {
+  return (
+    <div className="screen-container">
+      <Header />
+      <Banner />
+    </div>
+  );
+};
+
+export default AddStudent;
