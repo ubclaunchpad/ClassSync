3bb8538a5c46fdd6ce62885f0399a0ea569f46dd
Josh Fung <joshuagyfung@gmail.com>
2023-10-08 18:46:22 -0700

    Started using react-hook-form in user form

--- frontend/src/components/UserForm/index.js
+++ frontend/src/components/UserForm/index.js
@@ -1,29 +1,65 @@
 import "./index.css";
+import { useForm } from "react-hook-form";
 
 const UserForm = () => {
-  const onSubmit = () => {};
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-  // TODO: Use react-hook-forms
+  console.log(errors);
+
+  // TODO: Incomplete
   return (
     <div className="user-form-container">
       <h4 className="form-title">Sign Up</h4>
-      <form className="input-form">
-        <div className="input-container" onSubmit={onSubmit}>
+      <form
+        className="input-form"
+        onSubmit={handleSubmit((data) => {
+          console.log(data);
+        })}
+      >
+        <div className="input-container">
           <h5 className="input-title">Email</h5>
-          <input type="text" className="input" />
+          <input
+            type="text"
+            className="input"
+            {...register("email", { required: "Email is required!" })}
+          />
         </div>
         <div className="input-container">
           <h5 className="input-title">Password</h5>
-          <input type="text" className="input" />
+          <input
+            type="text"
+            className="input"
+            {...register("password", {
+              required: "Password is required!",
+              minLength: {
+                value: 8,
+                message: "Password must be at least 8 characters",
+              },
+            })}
+          />
         </div>
         <div className="input-container">
           <h5 className="input-title">Confirm Password</h5>
-          <input type="text" className="input" />
+          <input
+            type="text"
+            className="input"
+            {...register("confirmPassword", {
+              required: "Confirm password is required!",
+              minLength: {
+                value: 8,
+                message: "Confirm password must be at least 8 characters",
+              },
+            })}
+          />
         </div>
+        <button type="submit" className="submit-button">
+          Confirm Email
+        </button>
       </form>
-      <button type="submit" className="submit-button">
-        Confirm Email
-      </button>
     </div>
   );
 };
