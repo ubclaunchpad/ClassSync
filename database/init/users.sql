015cbe69afc65a82f86fe398606eb2135e54f1fc
Sue Lee <suuelee@outlook.com>
2023-10-10 01:11:30 -0700

    Add unique constraint to email in users table

--- database/init/users.sql
+++ database/init/users.sql
@@ -12,7 +12,7 @@ END IF;
 CREATE TABLE "users" (
     user_id SERIAL NOT NULL PRIMARY KEY,
     role userEnum NOT NULL,
-    email VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL UNIQUE,
     hashed_password VARCHAR(255)
 );
 
