57084c05e73e3b14a29b96c03402c2f4a4ad35c9
jjessieshang <17jessies@gmail.com>
2023-10-09 22:22:07 -0700

    add a student components

--- frontend/src/screens/addStudent/index.js
+++ frontend/src/screens/addStudent/index.js
@@ -13,16 +13,50 @@ const AddStudent = () => {
                 <h2>Column 1</h2>
             </div>
             <div class="column middle">
-                
                 <div className="student-info-container">
-                <h2>Column 2</h2>
+                <div class="header-row">
+                    <h2 className="add-student-header">Add a Student</h2>
+                    <button class="header-button">+ Add a New Student</button>
+                </div>
+                <form className="student-info-form">
+                    <div className="input-row">
+                        <label>
+                            First Name
+                            <input type="text" />
+                        </label>
+                        <label>
+                            Date of Birth
+                            <input type="text" />
+                        </label>
+                    </div>
+                    <div className="input-row">
+                        <label>
+                            Last Name
+                            <input type="text" />
+                        </label>
+                        <label>
+                            Student ID
+                            <input type="text" />
+                        </label>
+                    </div>
+                    <input type="submit" value="Confirm Student" />
+                </form>
+                <hr className="line"></hr>
                 </div>
                 <div className="student-info-container">
                 <h2>Column 2</h2>
                 </div>
             </div>
             <div class="column right">
-                <h2>Column 3</h2>
+                <div className="right-container">
+                    <h2>Column 3</h2>
+                </div>
+                <div className="right-container">
+                    <h2>Column 3</h2>
+                </div>
+                <div className="right-container">
+                    <h2>Column 3</h2>
+                </div>
             </div>
         </div>
     </div>
