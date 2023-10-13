dfc8f52c4f3091697a5d600497e187e862955adf
alecishuman <alec@pipeiq.ai>
2023-10-12 21:15:21 -0700

    dashboard page setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./screens/login";
 import AddStudent from "./screens/addStudent";
-import './App.css';
+import Dashboard from "./screens/dashboard";
+import "./App.css";
 
 function App() {
-return (
-  <div className="App">
-    <BrowserRouter>
-      <Routes>  
-        <Route path="/" element={<Login />} />
-        <Route path="/addStudent" element={<AddStudent />} />
-      </Routes>
-    </BrowserRouter>
-  </div>
-);
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/addStudent" element={<AddStudent />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
