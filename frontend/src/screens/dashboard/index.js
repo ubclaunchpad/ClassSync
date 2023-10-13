cf26fbc10165803bd3b5731d4ceeeeebe4074908
alecishuman <alec@pipeiq.ai>
2023-10-12 21:15:21 -0700

    dashboard page setup

---
+++ frontend/src/screens/dashboard/index.js
@@ -0,0 +1,11 @@
+import Header from "../../components/Header";
+import Banner from "../../components/Banner";
+
+export default function Dashboard() {
+  return (
+    <div className="screen-container">
+      <Header />
+      <Banner />
+    </div>
+  );
+}
