ba9a8a8841dbf80ee23f512c92453d2599e4d95d
alecishuman <alec@pipeiq.ai>
2023-10-14 12:22:08 -0700

    remade with dashboard component

--- frontend/src/screens/dashboard/index.js
+++ frontend/src/screens/dashboard/index.js
@@ -3,6 +3,7 @@ import "./index.css";
 import Header from "../../components/Header";
 import Banner from "../../components/Banner";
 import Student from "../../components/Student";
+import { ParentDashboardLayout } from "../../components/ParentDashboardLayout";
 
 import add from "../../assets/add.svg";
 import notifyClass from "../../assets/notify-bell.svg";
@@ -19,9 +20,23 @@ export default function Dashboard() {
 
   return (
     <div className="screen-container">
-      <Header />
-      <Banner />
-      <div className="main-screen">
+      <ParentDashboardLayout>
+        <div className="students">
+          <div className="students-header">
+            <div className="students-title">Students ({})</div>
+            <div className="add-student">
+              <img src={add} alt="add class" />
+              <div className="add-student-text">Add a New Student</div>
+            </div>
+          </div>
+          <div className="all-students">
+            {students.map((name) => (
+              <Student name={name} />
+            ))}
+          </div>
+        </div>
+      </ParentDashboardLayout>
+      {/* <div className="main-screen">
         <div className="left-bar">
           <div className="upcoming-classes-header">
             <div className="upcoming-classes-title">Upcoming Classes</div>
@@ -70,7 +85,7 @@ export default function Dashboard() {
             </div>
           </div>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
