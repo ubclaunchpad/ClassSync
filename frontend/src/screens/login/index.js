da6cd088c11ed4fbf2d9e2f068c1b4f68d75e818
Josh Fung <joshuagyfung@gmail.com>
2023-10-14 14:39:16 -0700

    Added sign up message for login page

--- frontend/src/screens/login/index.js
+++ frontend/src/screens/login/index.js
@@ -4,12 +4,21 @@ import LoginForm from "../../components/LoginForm";
 import "./index.css";
 
 const Login = () => {
+  // TODO: Redirect to sign up page
+  const signUpOnClick = () => {
+    console.log("Sign Up button clicked!");
+  };
+
   return (
     <div className="screen-container">
       <Header />
       <Banner />
       <div className="content-container">
         <LoginForm />
+        <div className="signup-container">
+          <h2>New to the Code Initiative?</h2>
+          <button onClick={signUpOnClick}>Sign Up</button>
+        </div>
       </div>
     </div>
   );
