d228447782c5bdd9ba7844b0d5e5e02ac207dc3a
tsomani <tabreek.somani@gmail.com>
2023-10-13 21:46:34 -0700

    Added tutor registration browser route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./screens/login";
 import AddStudent from "./screens/addStudent";
+import TutorRegistration from "./screens/tutorRegistration";
 import './App.css';
 
 function App() {
-return (
-  <div className="App">
-    <BrowserRouter>
-      <Routes>  
-        <Route path="/" element={<Login />} />
-        <Route path="/addStudent" element={<AddStudent />} />
-      </Routes>
-    </BrowserRouter>
-  </div>
-);
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/addStudent" element={<AddStudent />} />
+          <Route path="/tutorRegistration" element={<TutorRegistration />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
