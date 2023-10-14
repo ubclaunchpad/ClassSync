ff859786e92d141926c9dd9e471306717f556ab7
Josh Fung <joshuagyfung@gmail.com>
2023-10-14 15:03:42 -0700

    Added routes for sign up and confirmation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./screens/login";
+import SignUp from "./screens/signup";
+import Confirmation from "./screens/confirmation";
 import AddStudent from "./screens/addStudent";
-import './App.css';
+import "./App.css";
 
 function App() {
-return (
-  <div className="App">
-    <BrowserRouter>
-      <Routes>  
-        <Route path="/" element={<Login />} />
-        <Route path="/addStudent" element={<AddStudent />} />
-      </Routes>
-    </BrowserRouter>
-  </div>
-);
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/signUp" element={<SignUp />} />
+          <Route path="/confirmation" element={<Confirmation />} />
+          <Route path="/addStudent" element={<AddStudent />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
