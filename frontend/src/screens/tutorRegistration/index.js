90bac5ecd1c07b2c0600b7deb283b994956e0d91
tsomani <tabreek.somani@gmail.com>
2023-10-20 08:36:57 -0700

    Updated Tutor Registration

--- frontend/src/screens/tutorRegistration/index.js
+++ frontend/src/screens/tutorRegistration/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+
 import "./index.css";
 
 
@@ -17,13 +18,13 @@ const TutorRegistration = () => {
                 },
                 body: JSON.stringify({ userID, password }),
             });
-            // const response = await fetch(url); // Replace with your actual API endpoint
 
 
             if (response.status === 200) {
                 // User created successfully
                 const responseData = await response.json();
                 console.log('User created:', responseData);
+
                 // Handle success, e.g., show a success message or redirect the user
             } else {
                 // Handle any error conditions
@@ -37,37 +38,6 @@ const TutorRegistration = () => {
             // Handle the error, e.g., display an error message
         }
     };
-
-    const addBio = async () => {
-        try {
-            const response = await fetch(url + '/tutor/bio', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                // body: JSON.stringify({ userID, password }),
-            });
-            // const response = await fetch(url); // Replace with your actual API endpoint
-
-
-            if (response.status === 200) {
-                // User created successfully
-                const responseData = await response.json();
-                console.log('User created:', responseData);
-                // Handle success, e.g., show a success message or redirect the user
-            } else {
-                // Handle any error conditions
-                const errorData = await response.json();
-                console.error('User creation error:', errorData);
-                // Handle the error, e.g., display an error message
-            }
-        } catch (error) {
-            // Handle the error
-            console.error('Error creating user:', error);
-            // Handle the error, e.g., display an error message
-        }
-    };
-
     return (
         <div>
             <input
