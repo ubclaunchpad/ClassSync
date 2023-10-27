89461bd1d644346b7ee90738d24551d3af412924
tsomani <tabreek.somani@gmail.com>
2023-10-27 10:39:46 -0700

    merge main to branch

--- frontend/src/screens/addStudent/index.js
+++ frontend/src/screens/addStudent/index.js
@@ -1,23 +1,14 @@
 import Header from "../../components/Header";
 import Banner from "../../components/Banner";
-import SearchBar from "../../components/SearchBar";
-import AddStudentForm from "../../components/AddStudentForm";
-import Plus from "../../assets/plus.png";
-import Notification from "../../assets/notifications.png";
-import Bag from "../../assets/bag.png";
-
+import UserForm from "../../components/UserForm";
 import "./index.css";
-import { ParentDashboardLayout } from "../../components/ParentDashboardLayout";
 
-const AddStudent = (props) => {
+const AddStudent = () => {
   return (
-    <ParentDashboardLayout>
-      <div className="student-info-container">
-        <AddStudentForm />
-        <hr className="line"></hr>
-      </div>
-      <div className="student-info-container"></div>
-    </ParentDashboardLayout>
+    <div className="screen-container">
+      <Header />
+      <Banner />
+    </div>
   );
 };
 
