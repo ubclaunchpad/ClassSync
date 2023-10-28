8b03ab5eb8d30b83128de6f8bb8b9bb8cc662ec0
Kalm03 <kamranalam.ra@gmail.com>
2023-10-28 12:04:28 -0700

    added availability by id/name and future

--- backend/src/models/admin.js
+++ backend/src/models/admin.js
@@ -1,6 +1,6 @@
 export class admin {
-    getAvailability(id, result) {
-        //get the availability of specific admin user
+    getAvailabilityById(id, result) {
+        //get the availability of tutor by id
         con.query(`CALL getAvailabilitiesByTutor(?)`, [id], (err, res) => {
             if (err) {
                 console.log("error: ", err);
@@ -10,4 +10,36 @@ export class admin {
             }
         });
     }
+
+    getAllFutureAvailability(result) {
+        //get the availability in the future
+        con.query(`CALL getAvailabilitiesByDateRange(?)`, [Date.now()], (err, res) => {
+            if (err) {
+                console.log("error: ", err);
+                result(err, null);
+            } else {
+                result(null, res[0]);
+            }
+        });
+    }
+
+    getAvailabilityByName(result) {
+        //get the availability of tutor by name
+        id = "temp" //get id by tutor name
+        con.query(`CALL getAvailabilitiesByTutor(?)`, [id], (err, res) => {
+            if (err) {
+                console.log("error: ", err);
+                result(err, null);
+            } else {
+                result(null, res[0]);
+            }
+        });
+    }
+
+
+    // by course
+
+
+    // by tutor name
+
 }
