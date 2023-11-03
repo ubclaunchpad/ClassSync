d3dfd981310f9962f15959b7140b997284c1ab3f
Kalm03 <kamranalam.ra@gmail.com>
2023-11-03 16:11:44 -0700

    added avail by course, future

--- backend/src/models/admin.js
+++ backend/src/models/admin.js
@@ -13,7 +13,7 @@ export class admin {
 
     getAllFutureAvailability(result) {
         //get the availability in the future
-        con.query(`CALL getAvailabilitiesByDateRange(?)`, [Date.now()], (err, res) => {
+        con.query(`CALL getAvailabilitiesByDateRange(?,?)`, [Date.now(), new Date(8.64e15)], (err, res) => {
             if (err) {
                 console.log("error: ", err);
                 result(err, null);
@@ -23,23 +23,35 @@ export class admin {
         });
     }
 
-    getAvailabilityByName(result) {
-        //get the availability of tutor by name
-        id = "temp" //get id by tutor name
-        con.query(`CALL getAvailabilitiesByTutor(?)`, [id], (err, res) => {
+    getAvailabilityByCourse(course, result) {
+        //get tutors by course
+        con.query(`CALL getTutorsByCourse(?)`, [course], (err, res) => {
             if (err) {
                 console.log("error: ", err);
                 result(err, null);
             } else {
-                result(null, res[0]);
+                var ids = res[0].map(function (item) {
+                    return item.id;
+                });
+                
+                availabilities = [];
+                
+                ids.forEach(id => {
+                    availabilities.push(con.query(`CALL getAvailabilitiesByTutor(?)`, [id], (err, res) => {
+                        if (err) {
+                            console.log("error: ", err);
+                            result(err, null);
+                        } else {
+                            return res[0];
+                        }
+                    }));
+                });
+                
+                result(null, availabilities);
             }
         });
+
+        //get the availability of tutor by id
+
     }
-
-
-    // by course
-
-
-    // by tutor name
-
 }
