17f42f156af2088f53d0465f715f5448d0a148dd
tsomani <tabreek.somani@gmail.com>
2023-11-27 00:10:21 -0800

    Merge changes for backend/src/auth/authentication.js from Signup-Login-cool

---
+++ backend/src/auth/authentication.js
@@ -0,0 +1,35 @@
+import bcrypt from "bcrypt";
+
+/**
+ * Hashes a password to be saved in the database
+ * @param {*} password
+ */
+const hashPassword = (password) => {
+  const saltRounds = 14;
+  bcrypt
+    .hash(password, saltRounds)
+    .then((hash) => {
+      // save in db with username
+      // NO NEED TO SAVE SALT -- bcrypt figures it out along w the hash when comparing
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+/**
+ * Compares the given password with the stored hash to see if they match
+ * @param {*} givenPassword - Password given by user attempting to sign in
+ * @param {*} hash - Hashed password stored in DB
+ * @returns {boolean} - Indicates if given password matches stored password
+ */
+const comparePassword = (givenPassword, hash) => {
+  bcrypt
+    .compare(givenPassword, hash)
+    .then((result) => {
+      return result;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
