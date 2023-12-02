82017550450a56b2c2088bea31e99cc2ac6915d6
Josh Fung <joshuagyfung@gmail.com>
2023-12-02 12:38:34 -0800

    Brought over loginController from old branch

--- backend/src/controllers/parentAuthController.js
+++ backend/src/controllers/parentAuthController.js
@@ -1,53 +1,43 @@
-import { hashPassword, comparePassword } from "../auth/authentication.js"
+import { hashPassword, comparePassword } from "../auth/authentication.js";
 import parentAuth from "../models/parentAuth.js";
 
 export default class parentAuthController {
+  constructor() {
+    this.parent = new parentAuth();
+  }
 
-    constructor() {
-        this.parent = new parentAuth();
+  async signup(email, password) {
+    const hashedPassword = await hashPassword(password);
+    return new Promise((resolve, reject) => {
+      return this.parent
+        .createUser(email, hashedPassword)
+        .then((id) => {
+          resolve(id);
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  }
 
-    }
-
-    async signup(email, password) {
-        const hashedPassword = await hashPassword(password);
-        return new Promise((resolve, reject) => {
-            return this.parent.createUser(email, hashedPassword).then((id) => {
-                resolve(id);
-            }).catch((err) => {
-                reject(err);
+  login(email, password) {
+    return new Promise((resolve, reject) => {
+      return this.parent
+        .getPassword(email)
+        .then((hashedPassword) => {
+          if (email) {
+            return comparePassword(password, hashedPassword).then((result) => {
+              if (result) {
+                resolve({ email: email, role: "guardian" });
+              } else {
+                reject("Incorrect password");
+              }
             });
+          }
+        })
+        .catch((err) => {
+          reject(err);
         });
-
-
-    }
-
-    login(email, password) {
-        return new Promise((resolve, reject) => {
-            return this.parent.getPassword(email).then((hashedPassword) => {
-                if (email) {
-
-
-                    return comparePassword(password, hashedPassword).then((result) => {
-
-                        if (result) {
-                            resolve(email);
-                        }
-                        else {
-                            reject("Incorrect password");
-                        }
-
-                    })
-                }
-            })
-                .catch((err) => {
-                    reject(err);
-                });
-
-        });
-
-    }
+    });
+  }
 }
-
-
-
-
--- backend/src/routes/parentSignupRoute.js
+++ backend/src/routes/parentSignupRoute.js
@@ -4,32 +4,36 @@ const router = Router();
 const parent = new parentAuthController();
 
 router.get("/pingcheck", (_, res) => {
-    res.send("pong");
+  res.send("pong");
 });
 
-
 router.post("/signup", (req, res) => {
-    const email = req.query.email;
-    const password = req.query.password;
+  const email = req.query.email;
+  const password = req.query.password;
 
-
-    return parent.signup(email, password).then((id) => {
-        res.status(200).json({ id: id });
-    }).catch((err) => {
-        console.log(err);
-        res.status(500).send("Signup failed");
+  return parent
+    .signup(email, password)
+    .then((id) => {
+      res.status(200).json({ id: id });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send("Signup failed");
     });
 });
 
 router.post("/login", (req, res) => {
-    const email = req.query.email;
-    const password = req.query.password;
+  const email = req.query.email;
+  const password = req.query.password;
 
-    return parent.login(email, password).then((_email) => {
-        res.status(200).json({ email: "Login successful for " + email });
-    }).catch((err) => {
-        console.log(err);
-        res.status(500).send("Login failed");
+  return parent
+    .login(email, password)
+    .then((_email) => {
+      res.status(200).json(_email);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send("Login failed");
     });
 });
 export default router;
