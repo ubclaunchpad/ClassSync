b561517d00131a05eebd36710a537ff1f3ce85ea
tsomani <tabreek.somani@gmail.com>
2023-12-28 15:50:56 -0800

    Syntax update

--- backend/src/models/parentAuth.js
+++ backend/src/models/parentAuth.js
@@ -1,32 +1,32 @@
 import con from "../../index.js";
 
 export default class parentAuth {
-    async createUser(email, hashPassword, fname, lname) {
-        const client = await con.connect();
-        try {
-            return new Promise((resolve, reject) => {
-                client.query(
-                    'CALL insertUser($1, $2, $3, $4, $5, $6)',
-                    ['guardian', email, hashPassword, fname, lname, null],
-                    (error, results) => {
-                        if (error) {
-                            console.error('Error:', error);
-                            reject(error);
-                        } else {
-                            const user_id = results.rows[0].id;  // Retrieve user_id from results
-                            resolve(user_id);  // Resolve with the returned user_id
-                        }
-                    }
-                );
-            });
-        } catch (err) {
-            console.log(err);
-        }
-        finally {
-            client.release();
-        }
+  async createUser(email, hashPassword, fname, lname) {
+    const client = await con.connect();
+    try {
+      return new Promise((resolve, reject) => {
+        client.query(
+          'CALL insertUser($1, $2, $3, $4, $5, $6)',
+          ['guardian', email, hashPassword, fname, lname, null],
+          (error, results) => {
+            if (error) {
+              console.error('Error:', error);
+              reject(error);
+            } else {
+              const user_id = results.rows[0].id;  // Retrieve user_id from results
+              resolve(user_id);  // Resolve with the returned user_id
+            }
+          }
+        );
+      });
+    } catch (err) {
+      console.log(err);
+    }
+    finally {
+      client.release();
     }
   }
+
 
   async getPassword(email) {
     const client = await con.connect();
