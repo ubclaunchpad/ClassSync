4541d93d3c311fa6d4384f4d71fcd301f9b60705
tsomani <tabreek.somani@gmail.com>
2024-01-04 10:31:09 -0800

    Updated Appointment Calendar

--- frontend/src/screens/booking/index.js
+++ frontend/src/screens/booking/index.js
@@ -1,17 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Calendar from "../../components/BookingCalendar";
 import "./index.css"; // Import your custom styles
 const AppointmentCalendar = () => {
-    const [selectedSlot, setSelectedSlot] = React.useState(null);
 
-    const handleBookClick = (slot) => {
-        console.log(`Booking slot ${slot.start} - ${slot.end}`);
-        setSelectedSlot(null);
-    };
+
+
 
     return (
 
-        <Calendar onSelectSlot={setSelectedSlot} />
+        <Calendar />
 
 
     );
