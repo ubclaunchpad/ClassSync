68cf9f69896ae414a8fb286655b0e2371d80500c
tsomani <tabreek.somani@gmail.com>
2024-01-08 23:40:50 -0800

    Added student and booking retrieval

--- backend/src/controllers/studentProfileController.js
+++ backend/src/controllers/studentProfileController.js
@@ -3,51 +3,75 @@ import { StudentProfile } from "../models/studentProfile.js";
 
 export default class studentProfileController {
 
+  getBookings(student_id, course_id) {
+    const studentProfileModel = new StudentProfile();
+    return studentProfileModel.getBookings(student_id, course_id).then((response) => {
+      return response;
+    }
+    ).catch((err) => {
+      console.log(err);
+      throw err;
+      // return err;
+    });
+  }
 
-    getStudentProfile(id) {
-      return new Promise((resolve, reject) => {
-        const studentProfileModel = new StudentProfile();
-  
-        studentProfileModel.getStudentProfiles((err, result) => {
-          if (err) {
-            reject({ error: err });
-          }
-          resolve(result);
-        });
+  getStudents() {
+    const studentProfileModel = new StudentProfile();
+
+
+    return studentProfileModel.getStudents().then((response) => {
+      return response;
+    }
+    ).catch((err) => {
+      console.log(err);
+      // return err;
+    });
+  }
+
+  getStudentProfile(id) {
+    return new Promise((resolve, reject) => {
+      const studentProfileModel = new StudentProfile();
+
+      studentProfileModel.getStudentProfiles((err, result) => {
+        if (err) {
+          reject({ error: err });
+        }
+        resolve(result);
       });
-    }
+    });
+  }
 
-    saveStudentProfile(req) {
-        return new Promise((resolve, reject) => {
-            const studentProfileModel = new StudentProfile();
+  saveStudentProfile(req) {
+    return new Promise((resolve, reject) => {
+      const studentProfileModel = new StudentProfile();
 
-            const studentProfile = {
-                first_name: req.body.f_name,
-                last_name: req.body.l_name,
-                birthday: req.body.dob,
-                accomodations: req.body.accomodations,
-                fk_parent_id: req.body.fk_guardian_id
-            };
+      const studentProfile = {
+        first_name: req.body.f_name,
+        last_name: req.body.l_name,
+        birthday: req.body.dob,
+        accomodations: req.body.accomodations,
+        fk_parent_id: req.body.fk_guardian_id
+      };
 
-            studentProfileModel.insertStudentProfile(studentProfile, (err, result) => {
-                if (err) {
-                reject({ error: err });
-                }
-                resolve(result);
-            });
-        });
-    }
+      studentProfileModel.insertStudentProfile(studentProfile, (err, result) => {
+        if (err) {
+          reject({ error: err });
+        }
+        resolve(result);
+      });
+    });
+  }
 
-      deleteStudentProfile(id) {
-        return new Promise((resolve, reject) => {
-            const studentProfileModel = new StudentProfile();
-    
-            studentProfileModel.deleteStudentProfile(id, (err, result) => {
-                if (err) {
-                reject({ error: err });
-                }
-                resolve(result);
-            });
-        });
-    }
+  deleteStudentProfile(id) {
+    return new Promise((resolve, reject) => {
+      const studentProfileModel = new StudentProfile();
+
+      studentProfileModel.deleteStudentProfile(id, (err, result) => {
+        if (err) {
+          reject({ error: err });
+        }
+        resolve(result);
+      });
+    });
+  }
 }
