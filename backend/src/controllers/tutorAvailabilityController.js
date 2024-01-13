7ba6e85ff68a0c8bf6d374e556a27a2d011ded8e
tsomani <tabreek.somani@gmail.com>
2024-01-13 12:40:58 -0800

    Get Tutor By Courses + Availability By Tutors

--- backend/src/controllers/tutorAvailabilityController.js
+++ backend/src/controllers/tutorAvailabilityController.js
@@ -39,9 +39,75 @@ export default class tutorAvailabilityController {
             throw (err);
         });
     }
+
+    async getTutorByCourse(course_id) {
+        return this.tutor.getTutorByCourse(course_id).then((tutors) => {
+
+            return tutors;
+        }
+        ).catch((err) => {
+            console.log("Error getting schedule ", err)
+        });
+    }
     async getAvailableTutors(startDate, course_id) {
         return this.tutor.getAvailableTutors(startDate, course_id).then((availabilityData) => {
 
+            let availabilityHashmap = {};
+            let tutorIdNameMap = {}
+
+            // Loop through each tutor's availability
+            availabilityData.forEach(tutorData => {
+                Object.entries(tutorData.availability_times).forEach(([day, timeSlots]) => {
+                    day = parseInt(day); // Convert day to integer
+                    if (!(day in availabilityHashmap)) {
+                        availabilityHashmap[day] = {};
+                    }
+                    tutorIdNameMap[tutorData.tutor_id] = tutorData.tutor_name;
+
+                    timeSlots.sort(); // Sort the time slots in ascending order
+
+
+                    for (let i = 0; i < timeSlots.length - 1; i++) {
+                        // Check if the current time slot and the next one are consecutive
+                        let currentSlot = timeSlots[i];
+                        let nextSlot = timeSlots[i + 1];
+                        let currentSlotDate = new Date(`1970-01-01T${currentSlot}:00`);
+                        let nextSlotDate = new Date(`1970-01-01T${nextSlot}:00`);
+                        let diffMinutes = (nextSlotDate - currentSlotDate) / 1000 / 60;
+
+                        if (diffMinutes === 30) {
+                            if (!(currentSlot in availabilityHashmap[day])) {
+                                availabilityHashmap[day][currentSlot] = new Set();
+                            }
+
+                            // Add tutor_id to the set for the given time slot
+                            availabilityHashmap[day][currentSlot].add(tutorData.tutor_id);
+                        }
+                    }
+                });
+            });
+
+            // Convert each Set to an Array for JSON serialization
+            Object.values(availabilityHashmap).forEach(timeSlots => {
+                Object.keys(timeSlots).forEach(timeSlot => {
+                    timeSlots[timeSlot] = Array.from(timeSlots[timeSlot]);
+                });
+            });
+
+            // Filter out days with no available time slots
+            availabilityHashmap = Object.fromEntries(
+                Object.entries(availabilityHashmap).filter(([day, timeSlots]) => Object.keys(timeSlots).length > 0)
+            );
+
+            return { availabilityHashmap, tutorIdNameMap };
+        });
+    }
+
+    async getSelectedTutorsAvailability(startDate, tutor_ids) {
+        return this.tutor.getSelectedTutorsAvailability(startDate, tutor_ids).then((availabilityData) => {
+
+
+            console.log("Availability data ", availabilityData);
             let availabilityHashmap = {};
             let tutorIdNameMap = {}
 
