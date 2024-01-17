3c508af86becca6b7f21e05b4e4141f099066182
tsomani <tabreek.somani@gmail.com>
2024-01-16 17:43:45 -0800

    Added sorting to view enrollments from admin

--- frontend/src/screens/registrations/index.js
+++ frontend/src/screens/registrations/index.js
@@ -5,6 +5,12 @@ import { TutorDashboardLayout } from '../../components/TutorDashboardLayout';
 const Registrations = () => {
 
     const [registrations, setRegistrations] = useState([]);
+    const [sortedByGuardian, setSortedByGuardian] = useState(false);
+    const [sortDirection, setSortDirection] = useState('asc');
+    const [sortedByStudent, setSortedByStudent] = useState(false);
+    const [sortedByCourse, setSortedByCourse] = useState(false);
+    const [sortedByDate, setSortedByDate] = useState(false);
+    const [sortedByPaid, setSortedByPaid] = useState(false);
 
     const fetchRegistrations = async () => {
         const url = `http://localhost:8080/registrations`;
@@ -32,10 +38,101 @@ const Registrations = () => {
         }
     };
 
+    const resetSort = () => {
+        setSortedByGuardian(false);
+        setSortedByStudent(false);
+        setSortedByCourse(false);
+        setSortedByDate(false);
+        setSortedByPaid(false);
+    };
+    const handleSortByGuardian = () => {
+        resetSort();
+        const newSortDirection = sortDirection === 'asc' ? 'desc' : 'asc';
+        setSortDirection(newSortDirection);
+        setSortedByGuardian(true);
+
+        const sortedRegistrations = [...registrations];
+        sortedRegistrations.sort((a, b) => {
+            const guardianA = a.guardian.toLowerCase();
+            const guardianB = b.guardian.toLowerCase();
+            return newSortDirection === 'asc' ? guardianA.localeCompare(guardianB) : guardianB.localeCompare(guardianA);
+        });
+        setRegistrations(sortedRegistrations);
+    }
+
+    const handleByPaid = () => {
+        resetSort();
+        const newSortDirection = sortDirection === 'asc' ? 'desc' : 'asc';
+        setSortDirection(newSortDirection);
+        setSortedByPaid(true);
+
+        const sortedRegistrations = [...registrations];
+        sortedRegistrations.sort((a, b) => {
+            const paidA = Number(a.paid);
+            const paidB = Number(b.paid);
+            return newSortDirection === 'asc' ? paidA - paidB : paidB - paidA;
+        });
+        setRegistrations(sortedRegistrations);
+    }
+
+    const handleSortByStudent = () => {
+        resetSort();
+        const newSortDirection = sortDirection === 'asc' ? 'desc' : 'asc';
+        setSortDirection(newSortDirection);
+        setSortedByStudent(true);
+
+        const sortedRegistrations = [...registrations];
+        sortedRegistrations.sort((a, b) => {
+            const studentA = a.student.toLowerCase();
+            const studentB = b.student.toLowerCase();
+            return newSortDirection === 'asc' ? studentA.localeCompare(studentB) : studentB.localeCompare(studentA);
+        });
+        setRegistrations(sortedRegistrations);
+    }
+    const handleSortByCourse = () => {
+        resetSort();
+        const newSortDirection = sortDirection === 'asc' ? 'desc' : 'asc';
+        setSortDirection(newSortDirection);
+        setSortedByCourse(true);
+
+        const sortedRegistrations = [...registrations];
+        sortedRegistrations.sort((a, b) => {
+            const courseA = a.course.toLowerCase();
+            const courseB = b.course.toLowerCase();
+            return newSortDirection === 'asc' ? courseA.localeCompare(courseB) : courseB.localeCompare(courseA);
+        });
+        setRegistrations(sortedRegistrations);
+    }
+
+    const handleSortByDate = () => {
+        resetSort();
+        const newSortDirection = sortDirection === 'asc' ? 'desc' : 'asc';
+        setSortDirection(newSortDirection);
+        setSortedByDate(true);
+
+        const sortedRegistrations = [...registrations];
+        sortedRegistrations.sort((a, b) => {
+            const dateA = new Date(a.registration_date);
+            const dateB = new Date(b.registration_date);
+            return newSortDirection === 'asc' ? dateA - dateB : dateB - dateA;
+        });
+        setRegistrations(sortedRegistrations);
+    }
 
     return (
-        <TutorDashboardLayout>
-            <div style={{ display: 'flex', marginTop: '20px', marginLeft: '70%' }}>
+        <TutorDashboardLayout
+            rightColumnContent={
+                <div style={{ textAlign: "left", marginTop: "20px", marginRight: "15px" }}>
+                    <h3>Manage Student Enrollments</h3>
+
+                    <p>This dashboard allows administrators to manage student enrollments. View and sort enrollments by clicking on column headers.</p>
+                    <p>The "Paid" column indicates whether the enrollment has been paid for. You can toggle the payment status by clicking on the respective button in each row.</p>
+
+                    <p>
+                        Additionally, you can track the progress of each course, seeing how many classes are completed, booked, and pending respectively.
+                    </p>                </div>
+            }>
+            <div style={{ display: 'flex', marginTop: '20px', marginLeft: '60%' }}>
                 <div style={{ display: 'flex', alignItems: 'center', marginRight: '10px' }}>
                     <div style={{ width: '20px', height: '20px', backgroundColor: 'darkblue', marginRight: '5px', borderRadius: '3px' }} />
                     <div>Completed</div>
@@ -50,16 +147,44 @@ const Registrations = () => {
                 </div>
             </div>
 
-            <table style={{ width: '100%', borderCollapse: 'collapse', marginTop: '20px', marginLeft: '30px' }}>
+            <table style={{ width: '90%', borderCollapse: 'collapse', marginTop: '20px', marginLeft: '30px' }}>
                 <thead>
                     <tr style={{ borderBottom: '1px solid #000', backgroundColor: '#f2f2f2' }}>
-                        <th style={{ padding: '10px', textAlign: 'left', fontWeight: 'bold' }}>Enrollment ID</th>
-                        <th style={{ padding: '10px', textAlign: 'left', fontWeight: 'bold' }}>Guardian</th>
-                        <th style={{ padding: '10px', textAlign: 'left', fontWeight: 'bold' }}>Student</th>
-                        <th style={{ padding: '10px', textAlign: 'left', fontWeight: 'bold' }}>Course</th>
-                        <th style={{ padding: '10px', textAlign: 'left', fontWeight: 'bold' }}>Registration Date</th>
-                        <th style={{ padding: '10px', textAlign: 'left', fontWeight: 'bold' }}>Paid</th>
-                        <th style={{ padding: '10px', textAlign: 'left', fontWeight: 'bold' }}>Progress</th>
+                        <th style={{ padding: '10px', textAlign: 'left', fontWeight: 'bold' }}>ID</th>
+                        <th
+                            style={{ padding: '10px', textAlign: 'left', fontWeight: 'bold', cursor: 'pointer' }}
+                            onClick={handleSortByGuardian}
+                        >
+                            Guardian
+                            {sortedByGuardian && (sortDirection === 'asc' ? ' ▲' : ' ▼')}
+                        </th><th
+                            style={{ padding: '10px', textAlign: 'left', fontWeight: 'bold', cursor: 'pointer' }}
+                            onClick={handleSortByStudent}
+                        >
+                            Student
+                            {sortedByStudent && (sortDirection === 'asc' ? ' ▲' : ' ▼')}
+                        </th>
+                        <th
+                            style={{ padding: '10px', textAlign: 'left', fontWeight: 'bold', cursor: 'pointer' }}
+                            onClick={handleSortByCourse}
+                        >
+                            Course
+                            {sortedByCourse && (sortDirection === 'asc' ? ' ▲' : ' ▼')}
+                        </th>
+                        <th
+                            style={{ padding: '10px', textAlign: 'left', fontWeight: 'bold', cursor: 'pointer' }}
+                            onClick={handleSortByDate}
+                        >
+                            Registration Date
+                            {sortedByDate && (sortDirection === 'asc' ? ' ▲' : ' ▼')}
+                        </th>
+                        <th
+                            style={{ padding: '10px', textAlign: 'left', fontWeight: 'bold', cursor: 'pointer' }}
+                            onClick={handleByPaid}
+                        >
+                            Paid
+                            {sortedByPaid && (sortDirection === 'asc' ? ' ▲' : ' ▼')}
+                        </th>                       <th style={{ padding: '10px', textAlign: 'left', fontWeight: 'bold' }}>Progress</th>
 
                     </tr>
                 </thead>
