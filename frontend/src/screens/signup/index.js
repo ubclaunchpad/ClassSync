907f5e2a78a54c0831ca18e8d7f36cfb410eebd6
tsomani <tabreek.somani@gmail.com>
2024-01-27 18:49:39 -0800

    Updated Parent Signup Form

--- frontend/src/screens/signup/index.js
+++ frontend/src/screens/signup/index.js
@@ -2,16 +2,20 @@ import Header from "../../components/Header";
 import Banner from "../../components/Banner";
 import SignUpForm from "../../components/SignUpForm";
 import "./index.css";
+import RegisterUserForm from "../../components/RegisterUserForm";
+import { ParentDashboardLayout } from "../../components/ParentDashboardLayout";
 
 const Signup = () => {
   return (
-    <div className="screen-container">
-      <Header />
-      <Banner />
-      <div className="content-container">
-        <SignUpForm />
-      </div>
-    </div>
+
+<div className="screen-container">
+<Header />
+  <Banner />
+<div className='register-tutor-container'>
+<RegisterUserForm role='guardian' redirect='/' />
+</div >
+
+</div >
   );
 };
 
