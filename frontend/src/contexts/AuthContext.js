d9f4a34f6d9b7c685ba15610aba69c52d14fb244
tsomani <tabreek.somani@gmail.com>
2024-03-31 12:04:02 -0700

    Added Auth Context

---
+++ frontend/src/contexts/AuthContext.js
@@ -0,0 +1,40 @@
+// AuthContext.js
+import React, { createContext, useContext, useState, useEffect } from 'react';
+import { navigate } from 'react-big-calendar/lib/utils/constants';
+import { useNavigate } from 'react-router-dom';
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const navigate = useNavigate()
+
+  // Load user from localStorage on component mount
+  useEffect(() => {
+    const storedUser = localStorage.getItem('user');
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
+    }
+  }, []);
+
+  // Update localStorage whenever user changes
+  useEffect(() => {
+    localStorage.setItem('user', JSON.stringify(user));
+  }, [user]);
+
+  const login = (userData) => {
+    setUser(userData);
+  };
+
+  const logout = () => {
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
