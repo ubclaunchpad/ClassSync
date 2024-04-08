7ba7c045091388c36c8d233bca22081d4d73de3b
tsomani <tabreek.somani@gmail.com>
2024-04-07 17:39:15 -0700

    Added student enrollment

--- backend/src/routes/studentProfileRoute.js
+++ backend/src/routes/studentProfileRoute.js
@@ -9,6 +9,18 @@ router.get("/pingcheck", (_, res) => {
     res.status(200).json({ message: "pong" });
 });
 
+router.get("/enrollment", (req, res) => {
+  studentProfileController
+  .getStudentEnrollment(req.query.enrollment_id)
+  .then((response) => {
+    res.status(200).json(response);
+  })
+  .catch((err) => {
+    res.status(404).json(err);
+  });
+});
+
+
 router.get("/", (_, res) => {
   studentProfileController
     .getStudentProfile()
