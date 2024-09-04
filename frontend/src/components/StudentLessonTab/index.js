676ddaee5efa5bf885ab0dc6600962ed42f9c93b
Tabreek Somani <tabreek.somani@gmail.com>
2024-09-04 17:47:49 -0400

    Student Dashboard and Lesson Tab - Mobile Friendly

--- frontend/src/components/StudentLessonTab/index.js
+++ frontend/src/components/StudentLessonTab/index.js
@@ -5,7 +5,7 @@ import { Dropdown } from "primereact/dropdown";
 import { useEffect, useState } from "react";
 import "./index.css";
 
-export const StudentLessonTab = ({ course }) => {
+export const StudentLessonTab = ({ course, view = "pc" }) => {
   const { name, startDate, endDate, lessons, learningGoals } = course;
   // console.log(startDate)
 
@@ -123,156 +123,217 @@ export const StudentLessonTab = ({ course }) => {
     setShortStartDate(getShortDate(startDate));
   }, [lessons, startDate, endDate]);
   return (
-    <div className="student-dashboard__lesson-tab">
-      <div className="lesson-information">
-        <div className="lesson-information__learning-goals">
-          <div className="student-dashboard__title">
-            Learning Goals And Content Checklist
-          </div>
-          <div className="learning-goals-checklist__container">
-            {!learningGoals?.length > 0 && (
-              <div className="learning-goal__row">
-                No Learning Goals To Complete!
+    <>
+      {view === "pc" &&
+        <div className="student-dashboard__lesson-tab">
+          <div className="lesson-information">
+            <div className="lesson-information__learning-goals">
+              <div className="student-dashboard__title">
+                Learning Goals And Content Checklist
               </div>
-            )}
-            {learningGoals?.length > 0 &&
-              learningGoals.map((goal) => {
-                return (
+              <div className="learning-goals-checklist__container">
+                {!learningGoals?.length > 0 && (
                   <div className="learning-goal__row">
-                    <div className="learning-goal__checkbox">
-                      <Checkbox
-                        // disabled={true}
-                        checked={goal.completed}
-                      ></Checkbox>
-                    </div>
-                    {goal.goal}
-                  </div>
-                );
-              })}
-          </div>
-          {/* </div> */}
-        </div>
-        <div className="lesson-information__classes">
-          <div className="course-progress__container">
-            <div className="student-dashboard__title">Course Progress</div>
-            <div className="classes-information">
-              <div className="classes-graph__container">
-                <div className="classes-chart__container">
-                  <Chart
-                    height={"150px"}
-                    width={"150px"}
-                    className="student-classes__chart"
-                    type="pie"
-                    data={chartData}
-                    options={lightOptions}
-                  />
-                  <div className="class-complete__title">
-                    {`${completedClasses}/${incompletedClasses + completedClasses
-                      } Classes Complete! `}
-                  </div>
-                </div>
-
-                <div className="classes-start-end-date__container">
-                  <div>{"Start Date: " + shortStartDate}</div>
-                  <div>{"End Date: " + shortEndDate}</div>
-                </div>
-              </div>
-            </div>
-            <div className="classes__next-lesson__container">
-              <div className="student-dashboard__title">Lesson Links</div>
-              <div className="next-lesson__container">
-                {!lessons?.length > 0 && (
-                  <div className="next-lesson__date-row">
-                    <div>No Lessons Scheduled !</div>
+                    No Learning Goals To Complete!
                   </div>
                 )}
-                {lessons?.length > 0 &&
-                  lessons.map((lesson) => {
+                {learningGoals?.length > 0 &&
+                  learningGoals.map((goal) => {
                     return (
-                      <div className="next-lesson__date-row">
-                        <div>{getShortDateTime(lesson.startdate)}</div>
-                        <div>
-                          <a className="meeting-link" href={lesson.meetinglink}>
-                            Meeting Link
-                          </a>
+                      <div className="learning-goal__row">
+                        <div className="learning-goal__checkbox">
+                          <Checkbox
+                            // disabled={true}
+                            checked={goal.completed}
+                          ></Checkbox>
                         </div>
+                        {goal.goal}
                       </div>
                     );
                   })}
               </div>
+              {/* </div> */}
             </div>
-          </div>
-        </div>
-      </div>
-      <div className="course-files">
-        <div className="course-files__container">
-          <div className="student-dashboard__title">
-            Course Worksheets And Files
-          </div>
-          <div className="course-files-items__container">
-            <div className="course-files__search-row">
-              <Dropdown
-                className="course-files__dropdown p-dropdown-sm"
-                size="small"
-                optionLabel="name"
-                value={selectedLessonName}
-                options={lessonNames}
-                onChange={(e) => {
-                  console.log(e.value);
-                  setSelectedLessonName(e.value);
-                  if (e.value === "All Lessons") {
-                    setSelectedLessons([...lessons]);
-                  } else {
-                    const selectedLesson = lessons.filter(
-                      (lesson) => lesson.name === e.value
-                    );
-                    setSelectedLessons(selectedLesson);
-                  }
-                }}
-                placeholder="All Courses"
-              />
-              <Button
-                className="course-files__download-all "
-                onClick={() => {
-                  console.log("you clicked me!");
-                }}
-              >
-                Download All
-              </Button>
-            </div>
-            <div className="course-files-items">
-              {!selectedLessons?.length && (
-                <div className="lesson-files__container--empty">
-                  <div className="lesson-file__title">No Lesson Files</div>
-                </div>
-              )}
-              {selectedLessons?.length > 0 &&
-                selectedLessons.map((lesson, index) => {
-                  console.log("LKFJSLKD")
-                  console.log(lesson)
-                  return (
-                    <div className="lesson-file__container">
-                      <div className="lesson-file__title">{`${lesson.name}`}</div>
-                      <div className="lesson-files-items">
-                        {lesson.files.map((file) => {
-                          return (
-                            <div className="lesson-file__item">
-                              <a className="lesson-file__link" href={file.url}>
-                                <span className="lesson-file__link__text">
-                                  {file.name}
-                                </span>
-                              </a>
-                            </div>
-                          );
-                        })}
+            <div className="lesson-information__classes">
+              <div className="course-progress__container">
+                <div className="student-dashboard__title">Course Progress</div>
+                <div className="classes-information">
+                  <div className="classes-graph__container">
+                    <div className="classes-chart__container">
+                      <Chart
+                        height={"150px"}
+                        width={"150px"}
+                        className="student-classes__chart"
+                        type="pie"
+                        data={chartData}
+                        options={lightOptions}
+                      />
+                      <div className="class-complete__title">
+                        {`${completedClasses}/${incompletedClasses + completedClasses
+                          } Classes Complete! `}
                       </div>
                     </div>
-                  );
-                })}
+
+                    <div className="classes-start-end-date__container">
+                      <div>{"Start Date: " + shortStartDate}</div>
+                      <div>{"End Date: " + shortEndDate}</div>
+                    </div>
+                  </div>
+                </div>
+                <div className="classes__next-lesson__container">
+                  <div className="student-dashboard__title">Lesson Links</div>
+                  <div className="next-lesson__container" style={{ width: '80%' }}>
+                    {!lessons?.length > 0 && (
+                      <div className="next-lesson__date-row">
+                        <div>No Lessons Scheduled !</div>
+                      </div>
+                    )}
+                    {lessons?.length > 0 &&
+                      lessons.map((lesson) => {
+                        return (
+                          <div className="next-lesson__date-row">
+                            <div>{getShortDateTime(lesson.startdate)}</div>
+                            <div>
+                              <a className="meeting-link" href={lesson.meetinglink}>
+                                Meeting Link
+                              </a>
+                            </div>
+                          </div>
+                        );
+                      })}
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
-    </div>
+          <div className="course-files">
+            <div className="course-files__container">
+              <div className="student-dashboard__title">
+                Course Worksheets And Files
+              </div>
+              <div className="course-files-items__container">
+                <div className="course-files__search-row">
+                  <Dropdown
+                    className="course-files__dropdown p-dropdown-sm"
+                    size="small"
+                    optionLabel="name"
+                    value={selectedLessonName}
+                    options={lessonNames}
+                    onChange={(e) => {
+                      console.log(e.value);
+                      setSelectedLessonName(e.value);
+                      if (e.value === "All Lessons") {
+                        setSelectedLessons([...lessons]);
+                      } else {
+                        const selectedLesson = lessons.filter(
+                          (lesson) => lesson.name === e.value
+                        );
+                        setSelectedLessons(selectedLesson);
+                      }
+                    }}
+                    placeholder="All Courses"
+                  />
+                  <Button
+                    className="course-files__download-all "
+                    onClick={() => {
+                      console.log("you clicked me!");
+                    }}
+                  >
+                    Download All
+                  </Button>
+                </div>
+                <div className="course-files-items">
+                  {!selectedLessons?.length && (
+                    <div className="lesson-files__container--empty">
+                      <div className="lesson-file__title">No Lesson Files</div>
+                    </div>
+                  )}
+                  {selectedLessons?.length > 0 &&
+                    selectedLessons.map((lesson, index) => {
+                      console.log("LKFJSLKD")
+                      console.log(lesson)
+                      return (
+                        <div className="lesson-file__container">
+                          <div className="lesson-file__title">{`${lesson.name}`}</div>
+                          <div className="lesson-files-items">
+                            {lesson.files.map((file) => {
+                              return (
+                                <div className="lesson-file__item">
+                                  <a className="lesson-file__link" href={file.url}>
+                                    <span className="lesson-file__link__text">
+                                      {file.name}
+                                    </span>
+                                  </a>
+                                </div>
+                              );
+                            })}
+                          </div>
+                        </div>
+                      );
+                    })}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>}
+      {view === "mb" &&
+        <div className="student-dashboard__lesson-tab">
+          <div className="lesson-information">
+
+            <div className="lesson-information__classes">
+              <div className="course-progress__container" style={{ width: 'auto' }}>
+                <div className="student-dashboard__title">Course Progress</div>
+                <div className="classes-information">
+                  <div className="classes-graph__container">
+                    <div className="classes-chart__container">
+                      <Chart
+                        height={"150px"}
+                        width={"150px"}
+                        className="student-classes__chart"
+                        type="pie"
+                        data={chartData}
+                        options={lightOptions}
+                      />
+                      <div className="class-complete__title">
+                        {`${completedClasses}/${incompletedClasses + completedClasses
+                          } Classes Complete! `}
+                      </div>
+                    </div>
+
+                    <div className="classes-start-end-date__container">
+                      <div>{"Start Date: " + shortStartDate}</div>
+                      <div>{"End Date: " + shortEndDate}</div>
+                    </div>
+                  </div>
+                </div>
+                <div style={{ width: '100%' }}>
+                  <div className="student-dashboard__title">Lesson Links</div>
+                  <div style={{ backgroundColor: 'white', borderRadius: '10px', minWidth: '100%' }}>
+                    {!lessons?.length > 0 && (
+                      <div >
+                        <div>No Lessons Scheduled !</div>
+                      </div>
+                    )}
+                    {lessons?.length > 0 &&
+                      lessons.map((lesson) => {
+                        return (
+                          <div>
+                            <div>{getShortDateTime(lesson.startdate)}</div>
+                            <div>
+                              <a className="meeting-link" href={lesson.meetinglink}>
+                                Meeting Link
+                              </a>
+                            </div>
+                          </div>
+                        );
+                      })}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>}
+    </>
   );
 };
